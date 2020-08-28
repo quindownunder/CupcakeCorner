6682c3cb46f33172d5337d8a74f0018b3b71b472
Matthew Richardson <mat_richardson@mac.com>
2020-08-28 13:00:29 +1000

    Enabling forms

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -20,45 +20,27 @@ struct Result: Codable {
 
 struct ContentView: View {
     
-    @State private var results = [Result]()
+    @State private var username = ""
+    @State private var email = ""
     
-    var body: some View {
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                Text(item.collectionName)
-            }
-        }
-    .onAppear(perform: loadData)
+    var disableForm: Bool {
+        username.count < 5 || email.count < 5
     }
     
-    func loadData() {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("Invalid URL")
-            return
-        }
-        
-        let request = URLRequest(url: url)
-        
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            if let data = data {
-                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                    // we have good data – go back to the main thread
-                    DispatchQueue.main.async {
-                        // update our UI
-                        self.results = decodedResponse.results
-                    }
-
-                    // everything is good, so we can exit
-                    return
+    var body: some View {
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("email", text: $email)
+            }
+            
+            Section {
+                Button("Create Account") {
+                    print("Creating account...")
                 }
             }
-
-            // if we're still here it means there was a problem
-            print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
+            .disabled(disableForm)
         }
-        .resume()
     }
 }
 
